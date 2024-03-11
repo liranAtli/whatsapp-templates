fb4a8b13f3d5c65f099ad634853b7f90c53ea1a1
Liran Atli <liranatli@gmail.com>
2024-03-11 20:34:33 +0200

    finished s3 integration

--- handlers/upload-url.js
+++ handlers/upload-url.js
@@ -1,22 +1,68 @@
-'use strict';
+"use strict";
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const Joi = require("@hapi/joi");
 
+const client = new S3Client({
+  region: "eu-west-1",
+});
 
 module.exports.getSignedUrl = async (req, res) => {
+  if (!req.body || !req.pathParameters)
+    throw new Error(`Missing Parameter ${req.body} or ${req.pathParameters}`);
 
-    if (!req.body || !req.pathParameters) throw new Error(`Missing Parameter ${req.body} or ${req.pathParameters}`);
+  console.log(JSON.parse(req.body));
 
-    const schema = Joi.object({
-        user_id: Joi.string().required(),
-    });
+  try {
+    const { user_id } = req.pathParameters;
 
-    const {error, value} = schema.validate(req.body);
-
-    if (error) {
-        return handleError(HttpStatus.BAD_REQUEST, `[Template:Update:Error]:${HttpStatus[HttpStatus.BAD_REQUEST]}: Missing path parameter`);
+    if (!user_id) {
+      return handleError(
+        HttpStatus.BAD_REQUEST,
+        `[Upload:GetSignedUrl:Error]:${
+          HttpStatus[HttpStatus.BAD_REQUEST]
+        }: "Invalid parameter user_id"`
+      );
     }
 
+    const requestBody = JSON.parse(req.body);
 
+    const schema = Joi.object({
+      file_name: Joi.string().required(),
+    });
 
+    const { error, value } = schema.validate(requestBody);
 
-    return 'Hello World';
-}
+    console.log("Error: ", error);
+    console.log("Value: ", value);
+    if (!error) {
+      // Date in yyyy-mm-dd format
+      const date = new Date().toJSON().slice(0, 10);
+      // Create relative file path in user_id/data/file_name format
+      const filePath = `${user_id}/${date}/${value.file_name}`;
+
+      // Read s3 bucket name from environment variable RECIPIENT_S3_BUCKET_NAME
+      const bucketName = process.env.RECIPIENT_S3_BUCKET_NAME;
+
+      const params = {
+        Bucket: bucketName,
+        Key: filePath,
+        Expires: 6000, // Url is going to expire in 10 minute
+      };
+
+      const url = await getSignedUrl(
+        client,
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: filePath,
+        }),
+        { expiresIn: 6000 }
+      );
+    }
+
+    return { url };
+  } catch (err) {
+    console.log(err.message);
+    return err;
+  }
+};
